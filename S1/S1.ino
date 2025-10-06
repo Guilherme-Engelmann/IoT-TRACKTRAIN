a588c150ae38f20146e39a0e60eacb56246472b5
Guilherme Engelmann <guilherme_engelmann@estudante.sesisenai.org.br>
2025-10-06 09:58:04 -0300

    criação arquivo S1

--- S1/S1.ino
+++ S1/S1.ino
@@ -1,6 +1,17 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Wifi.h>
 
+const String SSID = "nome_da_rede";
+const String PASS = "senha_da_rede";
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Conectado ao Wifi");
+  Wifi.begin(SSID,PASS);
+  while(Wifi.status() != WL_CONNECTED){
+    Serial.print(".");
+    delay(200);
+  }
+  Serial.println("\nConectado com sucesso!");
 }
 
 void loop() {
