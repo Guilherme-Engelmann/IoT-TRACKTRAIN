d649afdccf72400987bfbcb8fe16e99b5f2cd8dd
BrinkerhoffDevCode <allyson_brinkerhoff@estudante.sesisenai.org.br>
2025-10-06 10:02:19 -0300

    Conexão com Wifi

--- S2/S2.ino
+++ S2/S2.ino
@@ -1,9 +1,22 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <WiFi.h>
 
+const String SSID = "FIESC_IOT_EDU";
+const String PASS = "8120gv08";
+
+void setup() {git
+  Serial.begin(115200);
+  Serial.println("Conectando ao WiFi");
+  WiFi.begin(SSID,PASS);
+  while(WiFi.status() != WL_CONNECTED){
+    Serial.println(".");
+    delay(200);
+  }
+  Serial.printlin("\nConectado com sucesso!");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+  //DOIT ESP32 DEVKIT
 
 }
