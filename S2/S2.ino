34e7339c0091d07ac63fd012c071b191e74191a2
BrinkerhoffDevCode <allyson_brinkerhoff@estudante.sesisenai.org.br>
2025-10-20 10:06:35 -0300

    eu fiz o broker

--- S2/S2.ino
+++ S2/S2.ino
@@ -1,7 +1,20 @@
 #include <WiFi.h>
+#include <PubSubClient.h>
+
+WiFiClient client;
+PubSubClient mqtt(client);
 
 const String SSID = "FIESC_IOT_EDU";
 const String PASS = "8120gv08";
+
+//constantes p/ broker
+const String URL    = "test.mosquitto.org";
+const int PORT      = 1883;
+const String USR    = "";
+const String broker_PASS   = "";
+const String Topic  = "DSM1";
+
+
 
 void setup() {
   Serial.begin(115200);
@@ -11,12 +24,33 @@ void setup() {
     Serial.println(".");
     delay(200);
   }
-  Serial.printlin("\nConectado com sucesso!");
+  Serial.println("\nConectado com sucesso!");
+  Serial.println("Conectando ao Broker");
+  mqtt.setServer(URL.c_str(),PORT);
+  while(!mqtt.connected()){
+    String ID = "S2_";
+    ID += String(random(0xffff),HEX); //cria a parte aleat.
+    mqtt.connect(ID.c_str(),USR.c_str(),broker_PASS.c_str());
+    Serial.print(".");
+    delay(200);
+  }
+  Serial.println("\nConectando com sucesso ao broker !");
 }
 
+
 void loop() {
+  String mensagem = "Allyson: "; //mudar p/ nome de vcs
+  mensagem += "alguma coisa";
+  
+  mqtt.publish(Topic.c_str(),mensagem.c_str());
+  mqtt.loop();
+  delay(1000);
+}
+
+
   // put your main code here, to run repeatedly:
 
   //DOIT ESP32 DEVKIT
+  //PubSubClient
 
-}
+
